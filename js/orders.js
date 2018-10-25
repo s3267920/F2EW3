6d8972e07455ba108008c20b1761fb37adb04dd0
rouca <s3267920@gmail.com>
2018-10-25 22:41:00 +0800

    表單連動完成

--- js/orders.js
+++ js/orders.js
@@ -1,3 +1,4 @@
+//選單部分
 (function() {
   let checkOption = document.querySelector('#check_more_option');
   let checkIcon = document.querySelector('#check_option_icon');
@@ -16,11 +17,9 @@
   let editSectionMenu = document.querySelector('#edit_section_menu');
   //下拉式選單
   editSectionOption.addEventListener('click', () => {
-    if (editSectionMenu.style.display === 'none') {
-      editSectionMenu.style.display = 'flex';
-    } else {
-      editSectionMenu.style.display = 'none';
-    }
+    editSectionMenu.style.display === 'none'
+      ? (editSectionMenu.style.display = 'flex')
+      : (editSectionMenu.style.display = 'none');
   });
   document.addEventListener('click', e => {
     //點擊其他地方後選單會隱藏
@@ -30,4 +29,72 @@
       checkMenu.style.display = 'none';
     }
   });
+})();
+//表格部分
+(function() {
+  let data = [
+    'Order ID',
+    'Customer',
+    'Product',
+    'Total',
+    'Add to Cart',
+    'Check-out',
+    'Address',
+    'Phone',
+    'Email',
+    'Status'
+  ];
+  let editSectionValue = document.querySelectorAll(
+    '#edit_section_menu >li >input'
+  );
+  let tableTitle = document.querySelectorAll('.table_title');
+  let tableTr = document.querySelectorAll('tr');
+  let tableContent = document.querySelectorAll('.table_content')[0].rows;
+
+  for (let i = 0; i < editSectionValue.length; i++) {
+    editSectionValue[i].addEventListener('click', () => {
+      //加入已勾選狀態
+      editSectionValue[i].checked
+        ? editSectionValue[i].setAttribute('checked', 'checked')
+        : editSectionValue[i].removeAttribute('checked', 'checked');
+      //勾選的話就顯示，不然就隱藏
+      for (let k = 1; k < tableTr.length; k++) {
+        if (editSectionValue[i].value == tableTr[0].cells[i + 1].textContent) {
+          // console.log(editSectionValue[i].value);
+          if (editSectionValue[i].checked) {
+            tableTr[0].cells[i + 1].style.display = 'inline-table';
+            tableTr[k].cells[i + 1].style.display = 'inline-table';
+          } else {
+            tableTr[0].cells[i + 1].style.display = 'none';
+            tableTr[k].cells[i + 1].style.display = 'none';
+          }
+        }
+      }
+    });
+    //如果選單沒勾選的話就隱藏
+    for (let k = 1; k < tableTr.length; k++) {
+      if (editSectionValue[i].value == tableTr[0].cells[i + 1].textContent) {
+        if (!editSectionValue[i].checked) {
+          tableTr[0].cells[i + 1].style.display = 'none';
+          tableTr[k].cells[i + 1].style.display = 'none';
+        }
+      }
+      if (tableTr[k].rowIndex % 2 == 0) {
+        tableTr[k].classList.add('lightgreen');
+      }
+      // console.log(tableTr[k]);
+    }
+  }
+
+  //Status狀態
+  let statusBtn = document.querySelectorAll('.status_Btn');
+  let statusList = document.querySelectorAll('.status_select');
+  for (let j = 0; j < statusBtn.length; j++) {
+    statusBtn[j].addEventListener('click', () => {
+      statusList[j].style.display === 'flex'
+        ? (statusList[j].style.display = 'none')
+        : (statusList[j].style.display = 'flex');
+    });
+  }
+  // console.log(tableTr);
 })();
