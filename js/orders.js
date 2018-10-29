d2decbeac439dbafcb27f2f2281262006a1b15f9
rouca <s3267920@gmail.com>
2018-10-29 22:16:44 +0800

    剩篩選器

--- js/orders.js
+++ js/orders.js
@@ -4,31 +4,111 @@
   let checkIcon = document.querySelector('#check_option_icon');
   let checkMenu = document.querySelector('#select_menu');
   let checkAllBox = document.querySelector('#all');
+  let statusBtn = document.querySelectorAll('.status_Btn');
+  let tableCheckbox = document.querySelectorAll('.table_check>input');
+  let tableLabel = document.querySelectorAll('.table_check>label');
+  let checkMenuA = document.querySelectorAll('#select_menu>li>a');
   //下箭頭選單
   checkOption.addEventListener('click', () => {
     checkMenu.style.display === 'none'
       ? (checkMenu.style.display = 'flex')
       : (checkMenu.style.display = 'none');
   });
+  //勾選自訂樣式
+  for (let id = 0; id < tableCheckbox.length; id++) {
+    let tableOrderId =
+      tableLabel[id].parentNode.parentNode.children[1].textContent;
+    let tableLabelId = 'check' + tableOrderId;
+    tableLabel[id].setAttribute('for', tableLabelId);
+    tableCheckbox[id].setAttribute('id', tableLabelId);
+    //label設置連動
+    tableLabel[id].addEventListener('click', () => {
+      let tableLabelCheckbox =
+        tableLabel[id].parentNode.parentNode.children[0].children[0];
+      tableLabelCheckbox.checked
+        ? tableLabelCheckbox.removeAttribute('checked', 'checked')
+        : tableLabelCheckbox.setAttribute('checked', 'checked');
+    });
+  }
   //勾選後出現標籤圖樣
   checkAllBox.addEventListener('click', () => {
-    checkMenu.style.display = 'flex';
-    checkAllBox.checked
-      ? (checkIcon.style.display = 'flex')
-      : (checkIcon.style.display = 'none');
-    checkMenu.style.display === 'none'
-      ? (checkMenu.style.display = 'flex')
-      : (checkMenu.style.display = 'none');
+    for (let j = 0; j < statusBtn.length; j++) {
+      checkMenu.style.display = 'none';
+      checkAllBox.checked
+        ? ((checkIcon.style.display = 'flex'),
+          //默認為全選
+          tableCheckbox[j].setAttribute('checked', 'checked'),
+          checkAllBox.setAttribute('checked', 'checked'))
+        : ((checkIcon.style.display = 'none'),
+          tableCheckbox[j].removeAttribute('checked', 'checked'),
+          checkAllBox.removeAttribute('checked', 'checked'));
+    }
   });
+
+  for (let cml = 0; cml < checkMenuA.length; cml++) {
+    checkMenuA[cml].addEventListener('click', function() {
+      //目前選取的狀態
+      for (let d = 0; d < checkMenuA.length; d++) {
+        switch (checkMenuA[cml].textContent) {
+          case 'Select All':
+          case 'Unselect All':
+          case 'Paid':
+          case 'Unpaid':
+          case 'Shipping':
+            checkMenuA[d].classList.remove('has_check'),
+              this.classList.add('has_check');
+            break;
+          default:
+            checkMenuA[d].classList.remove('has_check'),
+              this.classList.add('has_check');
+            break;
+        }
+      }
+      //依選取狀態來勾選
+      for (let j = 0; j < statusBtn.length; j++) {
+        let statusBtnCheckbox =
+          statusBtn[j].parentNode.parentNode.childNodes[1].children[0];
+        switch (checkMenuA[cml].textContent) {
+          case 'Select All':
+            tableCheckbox[j].setAttribute('checked', 'checked');
+            break;
+          case 'Unselect All':
+            tableCheckbox[j].removeAttribute('checked', 'checked');
+            break;
+          case 'Paid':
+            statusBtn[j].value === 'PAID'
+              ? statusBtnCheckbox.setAttribute('checked', 'checked')
+              : statusBtnCheckbox.removeAttribute('checked', 'checked');
+            break;
+          case 'Unpaid':
+            statusBtn[j].value === 'UNPAID'
+              ? statusBtnCheckbox.setAttribute('checked', 'checked')
+              : statusBtnCheckbox.removeAttribute('checked', 'checked');
+            break;
+          case 'Shipping':
+            statusBtn[j].value === 'SHIPPING'
+              ? statusBtnCheckbox.setAttribute('checked', 'checked')
+              : statusBtnCheckbox.removeAttribute('checked', 'checked');
+            break;
+          default:
+            statusBtn[j].value === 'DONE'
+              ? statusBtnCheckbox.setAttribute('checked', 'checked')
+              : statusBtnCheckbox.removeAttribute('checked', 'checked');
+            break;
+        }
+      }
+    });
+  }
   //點擊標籤出現選單
   checkIcon.addEventListener('click', () => {
     checkMenu.style.display === 'none'
       ? (checkMenu.style.display = 'flex')
       : (checkMenu.style.display = 'none');
   });
+  //右邊勾選選單
   let editSectionOption = document.querySelector('.edit_section > a');
   let editSectionMenu = document.querySelector('#edit_section_menu');
-  //右邊勾選選單
+
   editSectionOption.addEventListener('click', () => {
     editSectionMenu.style.display === 'none'
       ? (editSectionMenu.style.display = 'flex')
@@ -52,7 +132,6 @@
   let tableTr = document.querySelectorAll('tr');
   let tableTd = document.querySelectorAll('tr>td');
   let tableContent = document.querySelectorAll('.table_content')[0].rows;
-
   for (let i = 0; i < editSectionValue.length; i++) {
     editSectionValue[i].addEventListener('click', () => {
       //加入已勾選狀態
@@ -84,16 +163,13 @@
       if (tableTr[k].rowIndex % 2 == 0) {
         tableTr[k].classList.add('lightgreen');
       }
-      // console.log(tableTd);
     }
   }
-
   //Status狀態
   let statusLi = document.querySelectorAll('.status_select >a>li');
   let statusBtn = document.querySelectorAll('.status_Btn');
   let statusSpan = document.querySelectorAll('.status_Btn >span');
   let statusList = document.querySelectorAll('.status_select');
-
   for (let j = 0; j < statusBtn.length; j++) {
     statusBtn[j].addEventListener('click', () => {
       statusList[j].style.display === 'flex'
@@ -167,4 +243,6 @@
     color();
     // console.log(statusBtn[j]).parentElement.parentElement.children[i]);
   }
+  //換頁部分
+  let pageChangeUl = document.querySelector('.content_page');
 })();
