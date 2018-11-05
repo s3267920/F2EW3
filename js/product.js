4e8cd039980dba0c97731c85e9d96779c91fce66
rouca <s3267920@gmail.com>
2018-11-05 15:54:55 +0800

    暫時完成

--- js/product.js
+++ js/product.js
@@ -11,11 +11,16 @@
   //表格部分
   let statusBtn = document.querySelectorAll('.status_Btn');
   let selectOption = document.querySelectorAll('.status_option');
-  for (let tr = 1; tr < tableTr.length; tr++) {
-    if (tableTr[tr].rowIndex % 2 === 0) {
-      tableTr[tr].classList.add('lightgreen');
+  let trLightGreen = () => {
+    for (let tr = 1; tr < tableTr.length; tr++) {
+      if (tableTr[tr].rowIndex % 2 === 0) {
+        tableTr[tr].classList.add('lightgreen');
+      } else {
+        tableTr[tr].classList.remove('lightgreen');
+      }
     }
-  }
+  };
+
   for (let btn = 0; btn < statusBtn.length; btn++) {
     selectOption[btn].style.display = 'none';
     statusBtn[btn].addEventListener('click', () => {
@@ -48,17 +53,29 @@
     let selectBtn = selectOptionLi[a].parentNode.parentNode.parentNode;
     let selectLiBtn =
       selectOptionLi[a].parentNode.parentNode.parentNode.firstChild;
-
+    let selectOptionTr = selectOptionLi[a].closest('tr');
     selectOptionLi[a].addEventListener('click', () => {
-      selectOptionLi[a].textContent === 'Published'
-        ? ((selectBtn.value = 'PUBLISHED'),
-          (selectLiBtn.textContent = 'PUBLISHED'),
-          statusColor())
-        : ((selectBtn.value = 'UNPUBLISHED'),
-          (selectLiBtn.textContent = 'UNPUBLISHED'),
-          statusColor());
+      switch (selectOptionLi[a].textContent) {
+        case 'Published':
+          selectBtn.value = 'PUBLISHED';
+          selectLiBtn.textContent = 'PUBLISHED';
+          statusColor();
+          trLightGreen();
+          break;
+        case 'Unpublished':
+          selectBtn.value = 'UNPUBLISHED';
+          selectLiBtn.textContent = 'UNPUBLISHED';
+          statusColor();
+          trLightGreen();
+          break;
+        default:
+          selectOptionTr.remove();
+          trLightGreen();
+          break;
+      }
     });
   }
+  trLightGreen();
   statusColor();
   //check選單
   for (let i = 0; i < tableCheckboxLabel.length; i++) {
@@ -129,4 +146,159 @@
       });
     }
   }
+  //增加選項modal部分
+  let addLabel = document.querySelector('add_image>form>label');
+  let addInput = document.querySelector('#addImage');
+  let addInputId = addInput.getAttribute('id');
+  let preview = document.querySelector('.image_previews');
+
+  //照片上傳
+  function readURL(input) {
+    let files = input.files;
+    if (files && files[0])
+      for (let f = 0; f < files.length; f++) {
+        //需要放在循環裡，因為每次循環會重新製造一個新的
+        //放外面會報錯
+        let reader = new FileReader();
+        reader.addEventListener(
+          'load',
+          e => {
+            let image = new Image();
+            image.src = e.target.result;
+            image.alt = input.files[0].name;
+            preview.appendChild(image);
+          },
+          false
+        );
+        reader.readAsDataURL(addInput.files[f]);
+      }
+  }
+  addInput.addEventListener(
+    'change',
+    function() {
+      readURL(this);
+    },
+    false
+  );
+  let addBtn = document.querySelector('.add_product_btn');
+  let modal = document.querySelector('.add_modal');
+  let closeBtn = document.querySelector('.close_btn');
+  let specification = document.querySelector('.specification_info');
+  let addNewBtn = document.querySelector('.add_new_btn');
+  let specificationContent = `<label for="">
+  <span>Size</span>
+  <select name="" id="">
+    <option value="L">L</option>
+    <option value="M">M</option>
+    <option value="S">S</option>
+  </select>
+  </label>
+  <label for=""><span>Flavor</span><input type="text"></label>
+  <label for=""><span>Inventory</span><input type="text"></label>
+  `;
+  let saveDraft = document.querySelector('#save_draft');
+  addBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    specification.insertAdjacentHTML('beforeend', specificationContent);
+  });
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+    specification.innerHTML = '';
+    preview.innerHTML = '';
+  });
+  window.addEventListener('click', function(evt) {
+    if (evt.target == modal) {
+      modal.style.display = 'none';
+      specification.innerHTML = '';
+      preview.innerHTML = '';
+    }
+  });
+  //按下按鈕會加入新的規格
+
+  addNewBtn.addEventListener('click', () => {
+    specification.insertAdjacentHTML('beforeend', specificationContent);
+  });
+  let newTr = document.createElement('tr');
+  let publishedBtn = document.querySelector('#submit_btn');
+  publishedBtn.addEventListener('click', event => {
+    let trImage = preview.children[0];
+    let productName = document.querySelector('#product_name').value.trim();
+    let original = document.querySelector('#original').value.trim();
+    let discount = document.querySelector('#discount').value.trim();
+    let size = document.querySelector('.size').value;
+    if (preview.children.length === 0) {
+      alert('請傳照片');
+    } else {
+      let addTr = `
+      <td class="table_checkbox">
+      <input type="checkbox" name="" id="">
+      <label for=""></label>
+    </td>
+    <td class="product">
+      <img src="https://images.unsplash.com/photo-1514517220017-8ce97a34a7b6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7c376ffa487bcd258df29dc881b10703&auto=format&fit=crop&w=634&q=80">
+      <span>甜甜圈</span>
+    </td>
+    <td class="original">$500</td>
+    <td class="discount">$450</td>
+    <td class="information">
+      <ul>
+        <li>
+          <div class="size">L</div>
+          <ul class="flavor">
+            <li>香草</li>
+            <li>焦糖</li>
+          </ul>
+          <ul class="inventory">
+            <li>15</li>
+            <li>20</li>
+          </ul>
+        </li>
+        <li>
+          <div class="size">M</div>
+          <ul class="flavor">
+            <li>香草</li>
+            <li>焦糖</li>
+          </ul>
+          <ul class="inventory">
+            <li>15</li>
+            <li>20</li>
+          </ul>
+        </li>
+        <li>
+          <div class="size">S</div>
+          <ul class="flavor">
+            <li>香草</li>
+            <li>焦糖</li>
+          </ul>
+          <ul class="inventory">
+            <li>15</li>
+            <li>20</li>
+          </ul>
+        </li>
+      </ul>
+    </td>
+    <td class="status">
+      <button class="status_Btn" value="PUBLISHED">PUBLISHED<span class="triangle_bottom"></span>
+        <ul class="status_option">
+          <a href="javascript:;" class="Published">
+            <li>Published</li>
+          </a>
+          <a href="javascript:;" class="Unpublished">
+            <li>Unpublished</li>
+          </a>
+          <a href="javascript:;" class="delect">
+            <li>Delect</li>
+          </a>
+        </ul>
+      </button>
+
+    </td>`;
+
+      console.log(addTr);
+    }
+
+    console.log(preview.children.length);
+
+    event.preventDefault();
+  });
 })();
